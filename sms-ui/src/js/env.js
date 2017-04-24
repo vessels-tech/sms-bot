2bdc9df03d89552e794c81786577a41228610892
Lewis Daly <lewis.daly@enabled.com.au>
2017-04-24 23:24:25 +0930

    Trying to fix env stuff

--- sms-ui/src/js/env.js
+++ sms-ui/src/js/env.js
@@ -1,4 +1,3 @@
-
-/* Define env vars here. Webpack finds and replaces them */
-
-export const SMS_BOT_BASE_API = process.env.SMS_BOT_BASE_API;
+module.exports = {
+  SMS_BOT_BASE_API: process.env.SMS_BOT_BASE_API
+};

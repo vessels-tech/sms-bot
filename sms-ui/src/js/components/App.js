1f02bb0086d7ac11adbb9ae74f215c80b722313d
Lewis Daly <lewis.daly@enabled.com.au>
2017-05-03 10:26:55 +0930

    Added basic react router features

--- sms-ui/src/js/components/App.js
+++ sms-ui/src/js/components/App.js
@@ -4,6 +4,7 @@ import SMSBotService from '/client/SMSBotService';
 import ServiceLogContainer from  '/components/ServiceLog/ServiceLogContainer';
 import ServiceConfigurationContainer from '/components/ServiceConfiguration/ServiceConfigurationContainer';
 import { Button, ButtonGroup, Col, Grid, PageHeader, Row } from 'react-bootstrap';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
 
 class App extends Component {
   constructor(props) {
@@ -14,10 +15,10 @@ class App extends Component {
     };
   }
 
-  getServiceConfiguration() {
+  getServiceConfiguration(serviceId) {
     return (
       <ServiceConfigurationContainer
-        serviceId={'1'}
+        serviceId={serviceId}
         onChangeServiceId={(serviceId) => this.onChangeServiceId(serviceId)}
       />
     );
@@ -31,23 +32,43 @@ class App extends Component {
     });
   }
 
-  getServiceLogs() {
+  getServiceLogs(serviceId) {
     return (
       <ServiceLogContainer
-        serviceId={this.state.serviceId}
+        serviceId={serviceId}
         onChangeServiceId={(serviceId) => this.onChangeServiceId(serviceId)}
       />
     );
   }
 
-  render() {
-    console.log("app render");
+  getHome(props) {
+    let serviceId = props.params.serviceId ? props.params.serviceId : '1';
+
     return (
       <div className="container">
         <PageHeader>Welcome to the SMS-Bot</PageHeader>
-        {this.getServiceConfiguration()}
-        {this.getServiceLogs()}
+        <h3>Viewing Service: {serviceId}</h3>
+        {this.getServiceConfiguration(serviceId)}
+        {this.getServiceLogs(serviceId)}
       </div>
+    )
+  }
+
+  getAddress() {
+    return (
+      <h1>We are located at 555 Jackson St.</h1>
+    );
+  }
+
+  render() {
+    const NotFound = () => (<h1>404.. This page is not found!</h1>);
+
+    return (
+      <Router history={hashHistory}>
+        <Route path='/(:serviceId)' component={(props) => this.getHome(props)} />
+        <Route path='/address' component={() => this.getAddress()} />
+        <Route path='*' component={NotFound} />
+      </Router>
     )
   }
 }
